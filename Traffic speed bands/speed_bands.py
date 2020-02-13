36b5466e2232b9db5841d3071b787bb6f7169a5d
orionstar25 <chunnushrivastava@gmail.com>
2020-02-13 23:33:00 +0530

    Automate data collection

--- Traffic speed bands/speed_bands.py
+++ Traffic speed bands/speed_bands.py
@@ -1,50 +1,73 @@
+import requests
 import json
-import urllib.request, urllib.parse, urllib.error
-from urllib.parse import urlparse
-import httplib2 as http
+import datetime
+import os
+
 from credentials import *
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
+# Google Authentication for Drive
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
+
+# Authentication parameters
+headers = {
+			'AccountKey' : access_key,
+			'accept' : 'application/json'
+}
+
+
+def fetch_all(path):	
+
+	uri = 'http://datamall2.mytransport.sg/'
+	results = []
+
+	while True:
+		new_results = requests.get(
+			uri + path,
+			headers = headers,
+			params = {'$skip': len(results)} # 50 records/call
+		).json()['value']
+
+		if new_results == []:
+			break
+		else:
+			results += new_results
+
+	return results
+
+
+def get_folder_id(folder_name):
+    
+    # Get all folders and files in your Google Drive
+    fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    
+    for file in fileList:
+        if(file['title'] == folder_name):
+            return file['id']
 
 
 if __name__=="__main__":
 
-	#Authentication parameters
-	headers = { 'AccountKey' : access_key,
-	'accept' : 'application/json'} #this is by default
+	# Upload it into Google drive with name as: Date_Day_Time
+	x = datetime.datetime.now()
+	file_name = x.strftime("%c") + ".json"
 
-	#API parameters and specify type of API call
-	uri = 'http://datamall2.mytransport.sg/' #Resource URL
 	path = '/ltaodataservice/TrafficSpeedBandsv2'
-	skip = '?$skip='
-	method = 'GET'
-	body = ''
+	traffic_speed_bands = fetch_all(path) #58,780    
 
-	#Get handle to http
-	h = http.Http()
+	with open(file_name, "w") as f:
+		f.write(json.dumps(traffic_speed_bands, sort_keys=True, indent=4, ensure_ascii=False))
 
-	#Build query string
-	target = urlparse(uri + path)
-	i = 0
+	folder_id = get_folder_id("Traffic Speed Band Dataset")
+	file1 = drive.CreateFile({"mimeType": "text/plain", "parents": [{"kind": "drive#fileLink", "id": folder_id}]})
+	file1.SetContentFile(file_name)
+	file1.Upload()
+	print('Created file %s with mimeType %s' % (file1['title'], file1['mimeType']))
 
-	while i < 2001:
-		print(target.geturl())
-
-		#Obtain results
-		response, content = h.request(
-		target.geturl(),
-		method,
-		body,
-		headers)
-
-		#Parse JSON to print
-		jsonObj = json.loads(content)
-		# print(json.dumps(jsonObj, sort_keys=True, indent=4))
-
-		#Save result to file
-		with open("speed_bands.json","a+") as outfile:
-			#Saving jsonObj["d"]
-			json.dump(jsonObj, outfile, sort_keys=True, indent=4,
-			ensure_ascii=False)
-
-		#retrieve subsequent data
-		i += 500
-		target = urlparse(uri + path + skip + str(i))
+	# Delete local copy of file to save space
+	if os.path.exists(file_name):
+		os.remove(file_name)
